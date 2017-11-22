2435ea3a42567f8a918503895f813930c7c6bc1a
Unknown <traustid@gmail.com>
2017-11-22 10:10:52 +0100

    Sägenkarta kod

---
+++ scripts/app.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, hashHistory, Redirect } from 'react-router'
+
+import Application from './components/Application';
+import RecordListWrapper from './../ISOF-React-modules/components/views/RecordListWrapper';
+import RecordView from './../ISOF-React-modules/components/views/RecordView';
+import PlaceView from './../ISOF-React-modules/components/views/PlaceView';
+import PersonView from './../ISOF-React-modules/components/views/PersonView';
+
+console.log('Norska sägenkartan running React.js version '+React.version);
+
+// IE 11 backwards compatibility, Promise och Fetch
+import 'whatwg-fetch';
+import Promise from 'promise-polyfill';
+
+if (!window.Promise) {
+	window.Promise = Promise;
+}
+
+// Initalisera stöd för flerspråkighet
+import Lang from './../ISOF-React-modules/lang/Lang';
+window.Lang = Lang;
+window.l = Lang.get;
+
+// Initalisera React.js Router som bestämmer vilken "sida" användaren ser baserad på url:et
+ReactDOM.render(
+	<Router history={hashHistory}>
+		<Redirect from="/" to="/places"/>
+		<Route path="/" component={Application}>
+
+			<Route path="/places(/record_ids/:record_ids)(/search/:search)(/search_field/:search_field)(/type/:type)(/category/:category)(/year_from/:year_from)(/year_to/:year_to)(/person_relation/:person_relation)(/gender/:gender)(/person_landskap/:person_landskap)(/person_county/:person_county)(/person_harad/:person_harad)(/person_socken/:person_socken)(/page/:page)" 
+				manuallyOpenPopup="true" openButtonLabel="Visa sökträffar som lista" components={{popup: RecordListWrapper}}/>
+
+			<Route path="/place/:place_id(/record_ids/:record_ids)(/search/:search)(/search_field/:search_field)(/type/:type)(/category/:category)(/year_from/:year_from)(/year_to/:year_to)(/person_relation/:person_relation)(/gender/:gender)(/person_landskap/:person_landskap)(/person_county/:person_county)(/person_harad/:person_harad)(/person_socken/:person_socken)" 
+				components={{popup: PlaceView}}/>
+
+			<Route path="/person/:person_id" 
+				components={{popup: PersonView}}/>
+
+			<Route path="/record/:record_id(/record_ids/:record_ids)(/search/:search)(/search_field/:search_field)(/type/:type)(/category/:category)(/year_from/:year_from)(/year_to/:year_to)(/person_relation/:person_relation)(/gender/:gender)(/person_landskap/:person_landskap)(/person_county/:person_county)(/person_harad/:person_harad)(/person_socken/:person_socken)" 
+				components={{popup: RecordView}}/>
+
+		</Route>
+	</Router>,
+	document.getElementById('app')
+);
---
+++ scripts/config.js
@@ -0,0 +1,28 @@
+export default {
+	localLibraryName: 'sagenkarta_library',
+
+	requiredParams: {
+		type: 'arkiv,tryckt',
+		only_categories: 'true'
+	},
+
+	siteOptions: {
+
+	},
+
+	country: 'norway',
+
+	startPageUrl: '',
+
+	imageUrl: 'http://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/',
+	audioUrl: 'http://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/inspelningar/',
+
+	appUrl: 'http://www4.sprakochfolkminnen.se/sagner/',
+	siteUrl: 'http://www.sprakochfolkminnen.se/om-oss/kartor/sagenkartan.html',
+
+	apiUrl: 'http://frigg-test.sprakochfolkminnen.se/sagendatabas/api/es/',
+	djangoApiUrl: 'http://frigg-test.sprakochfolkminnen.se/sagendatabas/api/'
+//	apiUrl: 'http://uuc-isof004-t.its.uu.se/sagendatabas/api/es/'
+//	apiUrl: 'http://uuc-isof004-t.its.uu.se/sagendatabas/api/'
+//	apiUrl: 'http://localhost:8000/sagenkarta/'
+};
