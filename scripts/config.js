7bef2cf803b67ab589dbc6e1194cef4c8d90d695
Per Låås <per.laas@sprakochfolkminnen.se>
2019-03-07 16:31:33 +0100

    Web service calls with https.

--- scripts/config.js
+++ scripts/config.js
@@ -19,14 +19,14 @@ export default {
 
 	startPageUrl: '',
 
-	imageUrl: 'http://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/',
-	audioUrl: 'http://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/inspelningar/',
+	imageUrl: 'https://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/',
+	audioUrl: 'https://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/inspelningar/',
 
-	appUrl: 'http://www4.sprakochfolkminnen.se/sagner/norsk/',
-	siteUrl: 'http://www.hf.uio.no/ikos/tjenester/kunnskap/samlinger/norsk-folkeminnesamling/Sagnkart/sagnkart.html',
+	appUrl: 'https://www4.sprakochfolkminnen.se/sagner/norsk/',
+	siteUrl: 'https://www.hf.uio.no/ikos/tjenester/kunnskap/samlinger/norsk-folkeminnesamling/Sagnkart/sagnkart.html',
 
-	apiUrl: 'http://frigg.sprakochfolkminnen.se/sagendatabas/api/es/',
-	restApiUrl: 'http://frigg.sprakochfolkminnen.se/sagendatabas/api/'
+	apiUrl: 'https://frigg.sprakochfolkminnen.se/sagendatabas/api/es/',
+	restApiUrl: 'https://frigg.sprakochfolkminnen.se/sagendatabas/api/'
 //	apiUrl: 'http://uuc-isof004-t.its.uu.se/sagendatabas/api/es/'
 //	apiUrl: 'http://uuc-isof004-t.its.uu.se/sagendatabas/api/'
 //	apiUrl: 'http://localhost:8000/sagenkarta/'
